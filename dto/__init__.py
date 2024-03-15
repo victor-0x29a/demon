8c7541dad0f1c56ee4bbf243e031a2ab3757a13c
victor-0x29a <cartoonbr495@gmail.com>
2024-03-14 20:40:56 -0400

    Patch: deleted unecessary files

--- dto/__init__.py
+++
@@ -1,1 +0,0 @@
-from .applicationErrorDto import ApplicationError
--- dto/applicationErrorDto.py
+++
@@ -1,4 +0,0 @@
-class ApplicationError:
-    def __init__(self, reason, http_status_code=422):
-        self.reason = reason
-        self.http_status_code = http_status_code
