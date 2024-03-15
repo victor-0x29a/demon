9a9f8d265bb3c0adc9e82ee7aa4c2ef156feba37
victor-0x29a <cartoonbr495@gmail.com>
2024-03-15 10:07:03 -0400

    Patch: using the ipv4 regex to validate the ip_address

--- routers/client.py
+++ routers/client.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Query
+from typing import Annotated
 
 from models import UpdateHealthCheckUseCase
+from constants import IPV4_REGEX
+
 
 router = APIRouter(prefix="/client")
 
@@ -13,7 +16,7 @@ router = APIRouter(prefix="/client")
 
 
 @router.get("/health-check", status_code=204)
-async def update_health_check(request: Request, ip_address: str):
+async def update_health_check(request: Request, ip_address: Annotated[str, Query(regex=IPV4_REGEX)]):
     collection = request.app.database['host']
     update_use_case = UpdateHealthCheckUseCase(collection, ip_address)
     update_use_case.call()
