f070efc7445cf81cbba1fb56ea491cc316b84f36
victor-0x29a <cartoonbr495@gmail.com>
2024-03-15 10:42:53 -0400

    Fix: correct listener of events

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from pymongo import MongoClient
 
 from constants import (
@@ -10,16 +11,20 @@ from routers import ClientRouter
 
 app = FastAPI()
 
+main_app_lifespan = app.router.lifespan_context
 
-@app.on_event("startup")
-def startup_db():
+
+@asynccontextmanager
+async def lifespan_wrapper(app):
     app.mongodb_client = MongoClient(MONGO_URL)
     app.database = app.mongodb_client[MONGO_DB]
 
+    async with main_app_lifespan(app) as maybe_state:
+        yield maybe_state
 
-@app.on_event("shutdown")
-def shutdown_db():
     app.mongodb_client.close()
+
+app.router.lifespan_context = lifespan_wrapper
 
 
 app.include_router(ClientRouter)
