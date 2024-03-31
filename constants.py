a2a5c9e3f657aa15240dcfaf21637f0a1c3f240c
victor-0x29a <cartoonbr495@gmail.com>
2024-03-31 17:06:58 -0400

    Patch: new var to save the time of loop to send health-check from client

--- constants.py
+++ constants.py
@@ -8,3 +8,4 @@ MONGO_URL = config["MONGO_URL"]
 MONGO_DB = config["MONGO_DB"]
 
 IPV4_REGEX = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
+CLIENT_HEALTH_CHECK_LOOP_TIME = int(config["CLIENT_HEALTH_CHECK_LOOP_TIME"]) + 1
