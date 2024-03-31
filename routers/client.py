ddd0dcd3f437caacd73984ce787be553559dfe5b
victor-0x29a <cartoonbr495@gmail.com>
2024-03-30 22:57:42 -0400

    Feat: route to add a task

--- routers/client.py
+++ routers/client.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Request, Query
+from fastapi import APIRouter, Request, Query, Body
 from typing import Annotated
 
-from use_cases import UpdateHealthCheckUseCase
+from use_cases import UpdateHealthCheckUseCase, AddTaskUseCase
 from constants import IPV4_REGEX
 
 
@@ -20,3 +20,22 @@ async def update_health_check(request: Request, ip_address: Annotated[str, Query
     collection = request.app.database['host']
     update_use_case = UpdateHealthCheckUseCase(collection, ip_address)
     update_use_case.call()
+
+
+@router.post("/add-task", status_code=204)
+async def add_task(
+    request: Request,
+    ip_address: Annotated[str, Query(pattern=IPV4_REGEX)],
+    task_name: Annotated[str, Body()],
+    task_args: Annotated[list[str], Body()]
+):
+    collection = request.app.database['host']
+
+    add_task_use_case = AddTaskUseCase(
+        ip_address=ip_address,
+        task_name=task_name,
+        task_args=task_args or [],
+        host_collection=collection
+    )
+
+    add_task_use_case.call()
