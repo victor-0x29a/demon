c4c9dfadfef326715ae4ce4abdc5b3176d2787c3
victor-0x29a <cartoonbr495@gmail.com>
2024-03-31 16:57:50 -0400

    Feat: spec to date_in_range

--- specs/utils/test_datetime.py
+++ specs/utils/test_datetime.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from utils import now_to_str, str_to_date
+from utils import now_to_str, str_to_date, date_in_range
 
 
 def test_should_transform_now_to_str():
@@ -16,3 +16,13 @@ def test_should_transform_now_str_to_date():
     now_str = now_to_str(now)
 
     assert now == str_to_date(now_str)
+
+
+def test_should_in_range():
+    now = datetime.now()
+
+    simulated_date = datetime(now.year, now.month, now.day, now.hour, now.minute - 1)
+
+    max_minute_in_range = 2
+
+    assert date_in_range(simulated_date, max_minute_in_range)
