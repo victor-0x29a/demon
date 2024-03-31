729ebba5d424fc2daf8f1212956e3ce447ca3cd9
victor-0x29a <cartoonbr495@gmail.com>
2024-03-30 23:01:20 -0400

    Feat: specs to add task route

--- specs/integration/test_client.py
+++ specs/integration/test_client.py
@@ -1,11 +1,10 @@
-from . import app
-
 import pytest
+from . import app
 from faker import Faker
 from faker.providers import internet
 from fastapi.testclient import TestClient
 from mongomock import MongoClient
-from models import Host
+from models import Host, Task
 
 
 client = TestClient(app)
@@ -68,3 +67,64 @@ class TestHealthCheck:
         })
 
         assert old_client["health_check_datetime"] != new_client["health_check_datetime"]
+
+
+task_content = {
+    "task_name": "foo",
+    "task_args": ["bar"]
+}
+
+
+class TestTask:
+    def test_should_not_add_task_when_doesnt_exist_client(self, mocker, mock_mongodb):
+        mock_client = mocker.patch("fastapi.Request.app")
+
+        mock_client.database = {
+            "host": mock_mongodb
+        }
+
+        ip = fake.ipv4()
+
+        request = client.post(f"{namespace}/add-task?ip_address={ip}", json=task_content)
+
+        assert request.status_code == 422
+
+    def test_should_not_add_task_when_the_client_already_have_a_task(self, mocker, mock_mongodb):
+        mock_client = mocker.patch("fastapi.Request.app")
+
+        mock_client.database = {
+            "host": mock_mongodb
+        }
+
+        ip = fake.ipv4()
+
+        task = Task(name=task_content["task_name"], args=task_content["task_args"]).model_dump()
+
+        host = Host(_id=ip, task=task).model_dump()
+
+        mock_mongodb.insert_one(host)
+
+        request = client.post(f"{namespace}/add-task?ip_address={ip}", json=task_content)
+
+        assert request.status_code == 409
+
+    def test_should_add_a_task(self, mocker, mock_mongodb):
+        mock_client = mocker.patch("fastapi.Request.app")
+
+        mock_client.database = {
+            "host": mock_mongodb
+        }
+
+        ip = fake.ipv4()
+
+        host = Host(_id=ip).model_dump()
+
+        mock_mongodb.insert_one(host)
+
+        request = client.post(f"{namespace}/add-task?ip_address={ip}", json=task_content)
+
+        assert request.status_code == 204
+
+        host = mock_mongodb.find_one({"ip_address": ip})
+
+        assert host.get("task") is not {}
