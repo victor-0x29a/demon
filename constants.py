e4d7055f53134a7f3d869cf141fa35f3556f300a
victor-0x29a <cartoonbr495@gmail.com>
2024-04-04 13:07:36 -0400

    Patch: timezone var

--- constants.py
+++ constants.py
@@ -10,3 +10,5 @@ MONGO_DB = config["MONGO_DB"]
 
 IPV4_REGEX = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
 CLIENT_HEALTH_CHECK_LOOP_TIME = int(config["CLIENT_HEALTH_CHECK_LOOP_TIME"])
+
+TIMEZONE = config["TIMEZONE"]
