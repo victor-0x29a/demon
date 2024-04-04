34525d84db0e4dc5489a5aabf791c1706c5c0cbd
victor-0x29a <cartoonbr495@gmail.com>
2024-04-04 12:43:10 -0400

    Chore: loading the .env.test when is test environment

--- constants.py
+++ constants.py
@@ -1,11 +1,12 @@
+import os
 from dotenv import dotenv_values
 
+IS_TEST_ENVIRONMENT = bool(os.environ.get("FASTAPI_ENV") == "test")
 
-config = dotenv_values(".env")
-
+config = dotenv_values(".env" if not IS_TEST_ENVIRONMENT else ".env.test")
 
 MONGO_URL = config["MONGO_URL"]
 MONGO_DB = config["MONGO_DB"]
 
 IPV4_REGEX = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
-CLIENT_HEALTH_CHECK_LOOP_TIME = int(config["CLIENT_HEALTH_CHECK_LOOP_TIME"]) + 1
+CLIENT_HEALTH_CHECK_LOOP_TIME = int(config["CLIENT_HEALTH_CHECK_LOOP_TIME"])
