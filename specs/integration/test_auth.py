2cbb4941408110c31820b730dacc844be4c5738d
victor-0x29a <cartoonbr495@gmail.com>
2024-04-04 14:28:54 -0400

    Feat: specs to authentication route

---
+++ specs/integration/test_auth.py
@@ -0,0 +1,44 @@
+import pytest
+from . import app
+from fastapi.testclient import TestClient
+from mongomock import MongoClient
+import hashlib
+
+
+client = TestClient(app)
+
+namespace = "/auth"
+
+
+@pytest.fixture
+def mock_mongodb():
+    return MongoClient().db.collection
+
+
+class TestAuthentication:
+    def test_should_generate_a_token_with_valid_body(self):
+        response = client.post(f"{namespace}/gen", json={
+            "username": "admin",
+            "password": "admin"
+        })
+
+        assert response.status_code == 204
+        assert response.headers["Authorization"]
+
+    def test_should_reject_all_wrong_data(self):
+        payload_list = [
+            ["admin1", "admin"],
+            ["admin", "admin1"],
+            ["ADMIN", "admin"],
+            ["foo", "bar"],
+            ["admin ", "admin"],
+            [" admin ", " admin "]
+        ]
+
+        for payload in payload_list:
+            response = client.post(f"{namespace}/gen", json={
+                "username": payload[0],
+                "password": payload[1]
+            })
+
+            assert response.status_code == 406
