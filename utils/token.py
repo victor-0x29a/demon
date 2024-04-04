210e0b6fe24cc20ea0fc9eec529c18929d247ed9
victor-0x29a <cartoonbr495@gmail.com>
2024-04-04 13:26:08 -0400

    Feat: generate token util

--- utils/token.py
+++ utils/token.py
@@ -1,5 +1,20 @@
-from datetime import datetime, timezone
+import pytz
+from jose import jwt
+from datetime import datetime, timedelta
+from constants import TIMEZONE, SECRET
+
+timezone = pytz.timezone(TIMEZONE)
+
+algorithm = "HS256"
 
 
 def generate_token():
-    expire_datetime = datetime.now()
+    expire_datetime = timezone.localize(datetime.now()) + timedelta(minutes=10)
+
+    jwt_payload = {
+        "exp": expire_datetime
+    }
+
+    token = jwt.encode(jwt_payload, SECRET, algorithm=algorithm)
+
+    return token
