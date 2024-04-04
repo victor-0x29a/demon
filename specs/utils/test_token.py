32dd7a1d0bb624a4e88029875bba18e59c7ba717
victor-0x29a <cartoonbr495@gmail.com>
2024-04-04 14:59:55 -0400

    Feat: spec to token_is_valid

--- specs/utils/test_token.py
+++ specs/utils/test_token.py
@@ -1,4 +1,5 @@
-from utils import generate_token
+from utils import generate_token, token_is_valid
+from constants import SECRET
 
 
 def test_should_generate_token():
@@ -6,3 +7,13 @@ def test_should_generate_token():
 
     assert generated_token
     assert "str" in str(type(generated_token))
+
+
+def test_should_validate_token():
+    valid_token = generate_token()
+
+    invalid_token = "foo.bar"
+
+    assert token_is_valid(valid_token, SECRET)
+
+    assert not token_is_valid(invalid_token, SECRET)
